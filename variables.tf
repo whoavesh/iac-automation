289a1e19f547b9f365b7c06469ae9fc1ffd2ad80
Mr Cloud <127306466+whoavesh@users.noreply.github.com>
2025-04-23 17:34:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,43 @@
-variable "region" {
-    description = "AWS region"
-    default = "us-east-1"
+variable "keys"{
+    description = "ec2 keys"
+    default = "iac-keys"
+    type = string
+}
+
+variable "alb-sg-name" {
+    default = "alb-security-group"
+    type = string
+  
+}
+
+variable "ec2-sg-name" {
+  default = "ec2-security-group"
+  type = string
+}
+
+variable "alb-name" {
+    default = "main-alb"
+    type = string
+  
+}
+
+variable "lb-type" {
+  default = "application"
+  type = string
+}
+
+variable "alb-tg-name" {
+  default = "main-target-group"
+  type = string
+}
+
+variable "ami-id" {
+  default = "ami-0df368112825f8d8f"
+  type = string
+}
+
+
+variable "ec2_type" {
+  default = "t2.micro"
+  type = string
 }
