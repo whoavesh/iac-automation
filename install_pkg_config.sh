3535762d58581e3384467e515c9c616b2bf8b281
Mr Cloud <127306466+whoavesh@users.noreply.github.com>
2025-04-30 10:28:24 +0100

    Update install_pkg_config.sh

--- install_pkg_config.sh
+++ install_pkg_config.sh
@@ -14,12 +14,8 @@ sudo systemctl enable nginx
 sudo rm -rf /var/www/html/index.nginx-debian.html
 sudo rm -rf /usr/share/nginx/html/index.html
 
-# Set appropriate permissions for the web server
-sudo chown -R www-data:www-data /usr/share/nginx/html
-sudo chmod -R 755 /usr/share/nginx/html
-
 # Download index.html from GitHub raw content
-sudo wget -O /usr/share/nginx/html/ https://raw.githubusercontent.com/whoavesh/iac-automation/main/index.html
+sudo wget -O /usr/share/nginx/html/index.html https://raw.githubusercontent.com/whoavesh/iac-automation/main/index.html
 
 
 # Restart Nginx to apply changes
