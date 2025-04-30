0d711d42e09e4119b9c2a6fceb5baef96d9c9457
Mr Cloud <127306466+whoavesh@users.noreply.github.com>
2025-04-30 09:33:29 +0100

    Update install_pkg_config.sh

--- install_pkg_config.sh
+++ install_pkg_config.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
+# Update package manager
 sudo apt-get update -y
+
+# Install nginx
 sudo apt-get install nginx -y
+
+# Ensure nginx is running and enabled
 sudo systemctl start nginx
+sudo systemctl enable nginx
+
+#Clean Up
 sudo cd /var/www/html/
 sudo rm -rf index.html
+
+# Move index.html from Jenkins workspace to Nginx document root
 sudo mv /var/lib/jenkins/workspace/iac-pipeline/index.html /var/www/html/
+
+# Install Stress for cpu testing
 sudo apt-get install stress -y
